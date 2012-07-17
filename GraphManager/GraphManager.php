ba83343691521142ad42a6ac1bfa8ea6c5cd9b3e
Christophe Willemsen <willemsen.christophe@gmail.com>
2012-07-17 10:51:19 +0200

    removed temp conx test and added composer

--- GraphManager/GraphManager.php
+++ GraphManager/GraphManager.php
@@ -16,17 +16,5 @@ class GraphManager extends EntityManager
 	public function __construct(Client $client, MetaRepository $metaRepository)
 	{
 		parent::__construct($client, $metaRepository);
-
-		$this->testConnection();
-	}
-
-	public function testConnection()
-	{
-		try {
-			$this->getClient()->getReferenceNode();
-		} catch(\Exception $e) {
-			throw new \Exception(sprintf('Unable to connect to the graph db: %s', $e));
-		}
-		return;
 	}
 }
